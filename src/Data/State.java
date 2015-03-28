1a18e37bc45ffac6cc3fcccbe5914dac11f1a872
Ivo <ivaylokhr@gmail.com>
2015-03-28 14:48:33 +0000

    Some classes

--- src/Data/State.java
+++ src/Data/State.java
@@ -1,5 +1,27 @@
 package Data;
 
 public class State {
+	
+	private int military, culture, faith, economy;
+	
+	public State(int mil, int cul, int fai, int eco){
+		military = mil;	culture = cul;	faith = fai;	economy = eco;
+	}
+	
+	public int getMilitary(){
+		return military;
+	}
+	
+	public int getCulture(){
+		return culture;
+	}
+	
+	public int getFaith(){
+		return faith;
+	}
+	
+	public int getEconomy(){
+		return economy;
+	}
 
 }
