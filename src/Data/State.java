121d417d1399f709e04e86adfeea3e655e81a1b7
Rosen <ros95@mail.bg>
2015-03-28 15:36:37 +0000

    Fighting method, building method

--- src/Data/State.java
+++ src/Data/State.java
@@ -1,19 +1,35 @@
 package Data;
 
+import Data.Buildings.Building;
+
+import java.util.ArrayList;
+
 public class State {
 	
 	private int military, culture, faith, economy;
 	private String name;
+    private ArrayList<Building> buildings;
 	
 	public State(String name, int mil, int cul, int fai, int eco){
 		this.name = name;
+        buildings = new ArrayList<Building>();
 		military = mil; culture = cul;	faith = fai; economy = eco;
 	}
 	
 	public void fight(State other){
 		
 	}
-	
+
+    public void build(Building building){
+        buildings.add(building);
+    }
+
+    public void trainUnit(Unit unit){
+
+    }
+
+
+
 	//getting shit methods
 	public String getName(){
 		return name;
@@ -34,9 +50,21 @@ public class State {
 	public int getEconomy(){
 		return economy;
 	}
-	
+
+	public ArrayList<Building> getBuildings(){
+        return buildings;
+    }
+
 	public String toString(){
 		return name;
 	}
+
+
+    public static void main(String[] args){
+        State germany = new State("Germany", 1,1,1,1);
+        System.out.println(germany.getBuildings());
+        germany.build(new Building("Barracks"));
+        System.out.println(germany.getBuildings());
+    }
 
 }
--- src/Data/Unit.java
+++ src/Data/Unit.java
@@ -1,5 +1,36 @@
 package Data;
 
-public class Unit {
+import java.util.Random;
 
+public class Unit {
+    private int power;
+    private String name;
+
+
+
+    public Unit(String name){
+        this.name = name;
+        power = 10;
+    }
+    public void fight(Object otherObject){
+        if(otherObject instanceof Unit){
+            Unit otherUnit = (Unit) otherObject;
+            Random random = new Random();
+            int powerOne = random.nextInt(this.power);
+            int powerTwo = random.nextInt(otherUnit.power);
+            if(powerOne>=powerTwo){
+                System.out.println(name + " wins");
+            }
+            else{
+                System.out.println(otherUnit.name + " wins");
+            }
+        }
+
+    }
+
+
+    public static void main(String[] args){
+        Unit unit = new Unit("pesho");
+        unit.fight(new Unit("gosho"));
+    }
 }
